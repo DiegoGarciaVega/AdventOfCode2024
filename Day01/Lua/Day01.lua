e07cf47c705068e04fbd53c48b0d38dc2159f4df
DiegoGV <garciavdiego@uniovi.es>
2024-12-05 15:57:11 +0100

    [Lua] Day01

--- Day01/Lua/Day01.lua
+++ Day01/Lua/Day01.lua
@@ -1,20 +1,40 @@
-local open = io.open
+-- Open the input file
+local file = io.open("../input.txt", "r")
+print(file)
 
-local function readFile(path)
-    local file = open(path, "rb")
-    if not file then return nil end
-    local content = file:read "*a"
-    file:close()
-    return content
+-- Initialize lists
+local leftList = {}
+local rightList = {}
+
+-- Reading file and adding elements to lists
+for line in file:lines() do
+    local left, right = line:match("(%S+)%s+(%S+)")
+    leftList[#leftList + 1] = tonumber(left)
+    rightList[#rightList + 1] = tonumber(right)
 end
 
-local fileContent = readFile("../input.txt")
+file:close()
 
-local lines = {}
-for s in fileContent:gmatch("[^\r\n]+") do
-    table.insert(lines, s)
+-- Sorting the lists
+table.sort(leftList)
+table.sort(rightList)
+
+-- Calculate totalDistance and similarityScore
+local totalDistance, similarityScore = 0, 0
+for i = 1, #leftList do
+    local leftElement = leftList[i]
+    local rightElement = rightList[i]
+    totalDistance = totalDistance + math.abs(leftElement - rightElement)
+
+    -- Calculate similarityScore
+    local count = 0
+    for _, value in ipairs(rightList) do
+        if value == leftElement then
+            count = count + 1
+        end
+    end
+    similarityScore = similarityScore + count * leftElement
 end
 
-for i = 1, #lines do
-    print(lines[i])
-end
+-- Print results
+print(string.rep("#", 50) .. string.format("\nAdvent of Code 2024\n\t- Day 01\n\t\t★  Result: %d\n\t\t★★ Result: %d\n", totalDistance, similarityScore) .. string.rep("#", 50))
